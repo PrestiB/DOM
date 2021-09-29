edcad44ebad7f172af6b4fd6faeff549f5e53998
Bruno_P <presti.bruno88@gmail.com>
2021-09-29 15:47:03 +0200

    Intro

---
+++ Intro/app.js
@@ -0,0 +1,20 @@
+let docTitle = document.title;
+console.log(docTitle);
+
+docTitle = "Modifying the DOM";
+console.log(docTitle);
+
+for (let childNode of document.body.childNodes) {
+  console.log(childNode);
+}
+
+const background = () => {
+  let color = "rgb(";
+  for (let i = 0; i < 3; i++) {
+    if (i == 2) color += Math.floor(Math.random() * 256).toString() + ")";
+    else color += Math.floor(Math.random() * 256).toString() + ", ";
+  }
+  return color;
+};
+
+document.body.style.backgroundColor = background();
