646d60cf45624308318a6da1d00c5ed281ceb074
Bruno_P <presti.bruno88@gmail.com>
2021-09-30 16:09:04 +0200

    Selectors

--- Selectors/app.js
+++ Selectors/app.js
@@ -1,26 +1,27 @@
-let importantItems = document.querySelectorAll('.important');
+let importantItems = document.querySelectorAll(".important");
 for (item of importantItems) {
-    item.title = "This is an important item";
-    
-let images = document.querySelectorAll('img');
-for (item of images) {
-    if(item.classList.contains('important')) {
-        item.style.display = "none";
+  item.title = "This is an important item";
+
+  let images = document.querySelectorAll("img");
+  for (item of images) {
+    if (item.classList.contains("important")) {
+      item.style.display = "none";
     }
-}
+  }
 
+  const rand = (max) => {
+    return Math.floor(Math.random() * max + 1);
+  };
 
-const rand = (max) => {
-    return Math.floor(Math.random() * max +1);
-}
-
-let ps = document.querySelectorAll('p');
-for (item of ps) {
+  let ps = document.querySelectorAll("p");
+  for (item of ps) {
     if (item.classList.length > 0) {
-        console.log(`P : \nclassList = ${item.classList}\ntextContent = ${item.textContent}`);
+      console.log(
+        `P : \nclassList = ${item.classList}\ntextContent = ${item.textContent}`
+      );
+    } else {
+      console.log(`P : \ntextContent = ${item.textContent}`);
+      item.style.color = `rgb(${rand(225)},${rand(225)},${rand(225)})`;
     }
-    else {
-        console.log(`P : \ntextContent = ${item.textContent}`);
-        item.style.color = `rgb(${rand(225)},${rand(225)},${rand(225)})`;
-    }
+  }
 }
